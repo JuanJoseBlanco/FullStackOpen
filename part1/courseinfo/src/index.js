c53be3af9c9451d108e2123381cb2b93e2fdf5eb
Juan José Blanco <juan.blanco03@uptc.edu.co>
2021-06-01 18:54:47 -0500

    Added Excerices 1.1 & 1.2

---
+++ part1/courseinfo/src/index.js
@@ -0,0 +1,74 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1> {props.course} </h1>
+    </div>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.part[0]} />
+      <Part part={props.part[1]} />
+      <Part part={props.part[2]} />
+    </div>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises {props.total}</p>
+    </div>
+  );
+};
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>
+        {props.part.name} {props.part.exercises}
+      </p>
+    </div>
+  );
+};
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content part={course.parts} />
+      <Total
+        total={
+          course.parts[0].exercises +
+          course.parts[1].exercises +
+          course.parts[2].exercises
+        }
+      />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
